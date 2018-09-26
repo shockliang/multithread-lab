04ba13cb30aa1d7e6070d1bb26145a8f0e26c18e
shocklaing <shockwater@gmail.com>
2018-09-26 20:09:16 +0800

    Adding first demo project.

---
+++ FirstDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace FirstDemo
+{
+    class Program
+    {
+        public static void Write(char c)
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write(c);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            // Making new task vairabe and start it.
+            Task.Factory.StartNew(() => Write('.'));
+
+            var t = new Task(() => Write('?'));
+            t.Start();
+
+            Write('-');
+
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
