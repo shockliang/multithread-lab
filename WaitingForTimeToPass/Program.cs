325b9e3bd73c442ffddc57575c92f86378d7eb86
shocklaing <shockwater@gmail.com>
2018-09-29 00:06:17 +0800

    Diff way to making thread waiting.

--- WaitingForTimeToPass/Program.cs
+++ WaitingForTimeToPass/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace WaitingForTimeToPass
 {
@@ -6,7 +8,25 @@ namespace WaitingForTimeToPass
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var cts = new CancellationTokenSource();
+            var token = cts.Token;
+
+            var t = new Task(() =>
+            {
+                // Thread.Sleep(1000);
+                // Thread.SpinWait(1000);
+                // SpinWait.SpinUntil(() => true);
+
+                Console.WriteLine("Press any key to disarm; you have 5 seconds");
+                var cancelled = token.WaitHandle.WaitOne(5000);
+                Console.WriteLine(cancelled ? "Bomb disarmed." : "BOOM!!");
+            }, token);
+            t.Start();
+
+            Console.ReadKey();
+            cts.Cancel();
+            Console.ReadKey();
+
         }
     }
 }
