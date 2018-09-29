20e6ec8a62b537051bba082e45fd7523c7d6812d
shocklaing <shockwater@gmail.com>
2018-09-29 21:58:30 +0800

    Completed exception handling project.

--- ExceptionHandling/Program.cs
+++ ExceptionHandling/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace ExceptionHandling
 {
@@ -6,7 +7,52 @@ namespace ExceptionHandling
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                TestException();
+            }
+            catch (AggregateException ae)
+            {
+                foreach (var e in ae.InnerExceptions)
+                {
+                    Console.WriteLine($"Handled elsewhere: {e.GetType()}");
+                }
+            }
+
+            Console.WriteLine("Main program done.");
+            Console.ReadKey();
         }
+
+        private static void TestException()
+        {
+            var t1 = Task.Factory.StartNew(() =>
+            {
+                throw new InvalidOperationException("Can't do this!") { Source = "t1" };
+            });
+
+            var t2 = Task.Factory.StartNew(() =>
+            {
+                throw new AccessViolationException("Can't access this!") { Source = "t2" };
+            });
+
+            try
+            {
+                Task.WaitAll(t1, t2);
+            }
+            catch (AggregateException ae)
+            {
+                ae.Handle(e =>
+                {
+                    if (e is InvalidOperationException)
+                    {
+                        Console.WriteLine("Has been handle invlid operation");
+                        return true;
+                    }
+
+                    return false;
+                });
+            }
+        }
+
     }
 }
