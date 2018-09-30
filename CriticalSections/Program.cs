f4ef72cfa70478709ef910cb8ed7549005c1966d
shocklaing <shockwater@gmail.com>
2018-09-30 16:49:52 +0800

    Initial critical sections project.

---
+++ CriticalSections/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CriticalSections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -17,3 +17,5 @@
 ## Exception Handling ##
 
 
+## Critical Sections ##
+
