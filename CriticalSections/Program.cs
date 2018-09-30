20a2f3145527c7fc91a97c86cb99448daea2f20d
shocklaing <shockwater@gmail.com>
2018-09-30 17:36:57 +0800

    Using SpinLock for atomic operation.

--- CriticalSections/Program.cs
+++ CriticalSections/Program.cs
@@ -16,15 +16,12 @@ namespace CriticalSections
 
         public void Deposit(int amount)
         {
-            // += 
-            // op1: tmp <- get_Balance() + amount
-            // op2: set_Balance(tmp)
-            Interlocked.Add(ref balance, amount);
+            balance += amount;
         }
 
         public void Withdraw(int amount)
         {
-            Interlocked.Add(ref balance, -amount);
+            balance -= amount;
         }
     }
 
@@ -35,13 +32,24 @@ namespace CriticalSections
             var tasks = new List<Task>();
             var ba = new BankAccount();
 
+            var sl = new SpinLock();
+
             for (int i = 0; i < 10; i++)
             {
                 tasks.Add(Task.Factory.StartNew(() =>
                 {
                     for (int j = 0; j < 1000; j++)
                     {
-                        ba.Deposit(100);
+                        var lockTaken = false;
+                        try
+                        {
+                            sl.Enter(ref lockTaken);
+                            ba.Deposit(100);
+                        }
+                        finally
+                        {
+                            if (lockTaken) sl.Exit();
+                        }
                     }
                 }));
 
@@ -49,7 +57,16 @@ namespace CriticalSections
                 {
                     for (int j = 0; j < 1000; j++)
                     {
-                        ba.Withdraw(100);
+                        var lockTaken = false;
+                        try
+                        {
+                            sl.Enter(ref lockTaken);
+                            ba.Withdraw(100);
+                        }
+                        finally
+                        {
+                            if (lockTaken) sl.Exit();
+                        }
                     }
                 }));
             }
--- README.md
+++ README.md
@@ -24,4 +24,5 @@
 * `Interlocked.MemoryBarrier` shorthand for `Thread.MemoryBarrier()`.
     * Synchronizes memory access as follows.
 * `Interlocked.Exchange` sets a variable to a specified value as an atomic operation.
+* `SpinLock` object for atomic operation that until you are able to execute.
 
