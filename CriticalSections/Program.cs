3d983d158c1d6a5757e6885ef1f5b71035ded291
shocklaing <shockwater@gmail.com>
2018-10-01 21:51:52 +0800

    Lock app with mutex.

--- CriticalSections/Program.cs
+++ CriticalSections/Program.cs
@@ -33,79 +33,29 @@ namespace CriticalSections
 
     class Program
     {
-        private static SpinLock sl = new SpinLock(true);
-
+        private static Mutex mutex;
         static void Main(string[] args)
         {
-            var tasks = new List<Task>();
-            var ba = new BankAccount();
-            var ba2 = new BankAccount();
-
-            var mutex = new Mutex();
-            var mutex2 = new Mutex();
-
-            for (int i = 0; i < 10; i++)
+            const string appName = "MyApp";
+            var hasBeenCreated = false;
+            try
             {
-                tasks.Add(Task.Factory.StartNew(() =>
-                {
-                    for (int j = 0; j < 1000; j++)
-                    {
-                        bool haveLock = mutex.WaitOne();
-                        try
-                        {
-                            ba.Deposit(1);
-                        }
-                        finally
-                        {
-                            if (haveLock) mutex.ReleaseMutex();
-                        }
-                    }
-                }));
-
-                tasks.Add(Task.Factory.StartNew(() =>
-                {
-                    for (int j = 0; j < 1000; j++)
-                    {
-                        bool haveLock = mutex2.WaitOne();
-                        try
-                        {
-                            ba2.Deposit(1);
-                        }
-                        finally
-                        {
-                            if (haveLock) mutex2.ReleaseMutex();
-                        }
-                    }
-                }));
-
-                tasks.Add(Task.Factory.StartNew(() =>
-                {
-                    for (int j = 0; j < 1000; j++)
-                    {
-                        bool haveLock = WaitHandle.WaitAll(new[] { mutex, mutex2 });
-                        try
-                        {
-                            ba.Tranfer(ba2, 1);
-                        }
-                        finally
-                        {
-                            if(haveLock) 
-                            {
-                                mutex.ReleaseMutex();
-                                mutex2.ReleaseMutex();
-                            }
-                        }
-                    }
-                }));
+                mutex = Mutex.OpenExisting(appName);
+                Console.WriteLine($"Sorry, {appName} is already running");
             }
-
-            Task.WaitAll(tasks.ToArray());
-
-            Console.WriteLine($"Final balance 1 is {ba.Balance}");
-            Console.WriteLine($"Final balance 2 is {ba2.Balance}");
-
-            Console.WriteLine("Main program done.");
+            catch (WaitHandleCannotBeOpenedException)
+            {
+                Console.WriteLine("We can run the program just fine.");
+                mutex = new Mutex(false, appName, out hasBeenCreated);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            
             Console.ReadKey();
+            if(hasBeenCreated)
+                mutex.ReleaseMutex();
         }
     }
 }
