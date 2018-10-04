d2da2d1ac984e02b79b4b18feaa463029da73223
shocklaing <shockwater@gmail.com>
2018-10-04 23:24:19 +0800

    Continue with next task.

--- TaskCoordination/Program.cs
+++ TaskCoordination/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace TaskCoordination
 {
@@ -6,6 +7,18 @@ namespace TaskCoordination
     {
         static void Main(string[] args)
         {
+            var task = Task.Factory.StartNew(() =>
+            {
+                Console.WriteLine("Boling water");
+            });
+
+            var task2 = task.ContinueWith(t =>
+            {
+                Console.WriteLine($"completed task {t.Id}, pour water into cup.");
+            });
+
+            task2.Wait();
+
             Console.ReadKey();
         }
     }
