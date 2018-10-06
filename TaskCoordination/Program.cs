9814b96d2a0fc766fb59408b0784ff9af2314d9e
shocklaing <shockwater@gmail.com>
2018-10-06 13:18:53 +0800

    Barrier.

--- TaskCoordination/Program.cs
+++ TaskCoordination/Program.cs
@@ -6,42 +6,42 @@ namespace TaskCoordination
 {
     class Program
     {
+        private static Barrier barrier = new Barrier(2, b =>
+        {
+            Console.WriteLine($"Phase {b.CurrentPhaseNumber} is finished");
+        });
+
+        public static void Water()
+        {
+            Console.WriteLine("Putting the kettle on (takes a bit longer)");
+            Thread.Sleep(2000);
+            barrier.SignalAndWait(); // step2. counter = 2. 
+            Console.WriteLine("Pouring water into cup"); // step3. counter = 0. reset the counter.
+            barrier.SignalAndWait(); // step4. counter = 1 
+            Console.WriteLine("Putting the kettle away!");
+        }
+
+        public static void Cup()
+        {
+            Console.WriteLine("Finding the nicest cup of tea (fast)");
+            barrier.SignalAndWait(); // step1. counter = 1
+            Console.WriteLine("Adding tea.");
+            barrier.SignalAndWait(); // step5. counter = 2
+            Console.WriteLine("Adding sugar.");
+        }
+
         static void Main(string[] args)
         {
-            var parent = new Task(() =>
+            var water = Task.Factory.StartNew(Water);
+            var cup = Task.Factory.StartNew(Cup);
+
+            var tea = Task.Factory.ContinueWhenAll(new[] { water, cup }, tasks =>
             {
-                var child = new Task(() =>
-                {
-                    Console.WriteLine("Child task starting.");
-                    Thread.Sleep(3000);
-                    // throw new Exception();
-                    Console.WriteLine("Child task finshing.");
-                }, TaskCreationOptions.AttachedToParent);
-
-                var completionHandle = child.ContinueWith(t =>
-                {
-                    Console.WriteLine($"Horray! Task {t.Id}'s state is {t.Status}");
-                }, TaskContinuationOptions.AttachedToParent |
-                    TaskContinuationOptions.OnlyOnRanToCompletion);
-
-                var failHandler = child.ContinueWith(t =>
-                {
-                    Console.WriteLine($"Oops! task {t.Id}'s state is {t.Status}");
-                }, TaskContinuationOptions.AttachedToParent |
-                    TaskContinuationOptions.OnlyOnFaulted);
-
-                child.Start();
+                Console.WriteLine("Enjoy your cup of tea.");
             });
-            parent.Start();
 
-            try
-            {
-                parent.Wait();
-            }
-            catch (AggregateException ae)
-            {
-                ae.Handle(e => true);
-            }
+            tea.Wait();
+
             Console.ReadKey();
         }
     }
