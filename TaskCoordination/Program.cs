5ed2491d3ae7a58590bfe7c977af82dc1eeed5a7
shocklaing <shockwater@gmail.com>
2018-10-06 14:19:31 +0800

    AutoResetEvent.

--- TaskCoordination/Program.cs
+++ TaskCoordination/Program.cs
@@ -9,19 +9,29 @@ namespace TaskCoordination
     {
         static void Main(string[] args)
         {
-            var evt = new ManualResetEventSlim();
+            var evt = new AutoResetEvent(false); // Defautl: false
 
             Task.Factory.StartNew(() =>
             {
                 Console.WriteLine("Boiling water");
-                evt.Set();
+                evt.Set(); // set to true
             });
 
             var makeTea = Task.Factory.StartNew(() =>
             {
                 Console.WriteLine("Waiting for water...");
-                evt.Wait();
+                evt.WaitOne(); // set to false and false
                 Console.WriteLine("Here is your tea");
+                var ok = evt.WaitOne(1000); // set to false with timeout
+                if(ok)
+                {
+                    Console.WriteLine("Enjoy your tea");
+                }
+                else 
+                {
+                    // Because no one to set to true so it blocked 
+                    Console.WriteLine("No tea for you"); 
+                }
             });
 
             makeTea.Wait();
