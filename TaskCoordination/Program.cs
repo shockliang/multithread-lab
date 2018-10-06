637a422937b842faecd263ae6d42ffac5ccc5b70
shocklaing <shockwater@gmail.com>
2018-10-06 13:47:40 +0800

    CountdownEvent.

--- TaskCoordination/Program.cs
+++ TaskCoordination/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -6,42 +7,31 @@ namespace TaskCoordination
 {
     class Program
     {
-        private static Barrier barrier = new Barrier(2, b =>
-        {
-            Console.WriteLine($"Phase {b.CurrentPhaseNumber} is finished");
-        });
-
-        public static void Water()
-        {
-            Console.WriteLine("Putting the kettle on (takes a bit longer)");
-            Thread.Sleep(2000);
-            barrier.SignalAndWait(); // step2. counter = 2. 
-            Console.WriteLine("Pouring water into cup"); // step3. counter = 0. reset the counter.
-            barrier.SignalAndWait(); // step4. counter = 1 
-            Console.WriteLine("Putting the kettle away!");
-        }
-
-        public static void Cup()
-        {
-            Console.WriteLine("Finding the nicest cup of tea (fast)");
-            barrier.SignalAndWait(); // step1. counter = 1
-            Console.WriteLine("Adding tea.");
-            barrier.SignalAndWait(); // step5. counter = 2
-            Console.WriteLine("Adding sugar.");
-        }
+        private static int taskCount = 5;
+        private static Random random = new Random();
+        static CountdownEvent cte = new CountdownEvent(taskCount);
 
         static void Main(string[] args)
         {
-            var water = Task.Factory.StartNew(Water);
-            var cup = Task.Factory.StartNew(Cup);
-
-            var tea = Task.Factory.ContinueWhenAll(new[] { water, cup }, tasks =>
+            for (int i = 0; i < taskCount; i++)
             {
-                Console.WriteLine("Enjoy your cup of tea.");
+                Task.Factory.StartNew(() =>
+                {
+                    Console.WriteLine($"Entering task {Task.CurrentId}");
+                    Thread.Sleep(random.Next(3000));
+                    cte.Signal();
+                    Console.WriteLine($"Exiting task {Task.CurrentId}");
+                });
+            }
+
+            var finalTask = Task.Factory.StartNew(() =>
+            {
+                Console.WriteLine($"Waiting for other tasks to comlete in {Task.CurrentId}");
+                cte.Wait();
+                Console.WriteLine("All tasks completed");
             });
 
-            tea.Wait();
-
+            finalTask.Wait();
             Console.ReadKey();
         }
     }
