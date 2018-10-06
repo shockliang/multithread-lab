bb2ed0773b5644e5ad834d57b937fb5c471e08ca
shocklaing <shockwater@gmail.com>
2018-10-06 13:56:41 +0800

    ManualResetEventSlim.

--- TaskCoordination/Program.cs
+++ TaskCoordination/Program.cs
@@ -7,31 +7,25 @@ namespace TaskCoordination
 {
     class Program
     {
-        private static int taskCount = 5;
-        private static Random random = new Random();
-        static CountdownEvent cte = new CountdownEvent(taskCount);
-
         static void Main(string[] args)
         {
-            for (int i = 0; i < taskCount; i++)
-            {
-                Task.Factory.StartNew(() =>
-                {
-                    Console.WriteLine($"Entering task {Task.CurrentId}");
-                    Thread.Sleep(random.Next(3000));
-                    cte.Signal();
-                    Console.WriteLine($"Exiting task {Task.CurrentId}");
-                });
-            }
+            var evt = new ManualResetEventSlim();
 
-            var finalTask = Task.Factory.StartNew(() =>
+            Task.Factory.StartNew(() =>
             {
-                Console.WriteLine($"Waiting for other tasks to comlete in {Task.CurrentId}");
-                cte.Wait();
-                Console.WriteLine("All tasks completed");
+                Console.WriteLine("Boiling water");
+                evt.Set();
             });
 
-            finalTask.Wait();
+            var makeTea = Task.Factory.StartNew(() =>
+            {
+                Console.WriteLine("Waiting for water...");
+                evt.Wait();
+                Console.WriteLine("Here is your tea");
+            });
+
+            makeTea.Wait();
+
             Console.ReadKey();
         }
     }
