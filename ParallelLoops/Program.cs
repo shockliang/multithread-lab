9cfa42890bf77f40638b96045497f81ef4fbc23a
shocklaing <shockwater@gmail.com>
2018-10-07 18:15:12 +0800

    Partitioning.

--- ParallelLoops/Program.cs
+++ ParallelLoops/Program.cs
@@ -1,31 +1,50 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
 
 namespace ParallelLoops
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            int sum = 0;
-
-            Parallel.For(1, 1001,
-            () => 0,
-            (x, state, tls) =>
-            {
-                tls += x;
-                Console.WriteLine($"Task {Task.CurrentId} has sum {tls}");
-                return tls;
-            },
-            partialSum =>
-            {
-                Console.WriteLine($"Partial value of task {Task.CurrentId} is {partialSum}");
-                Interlocked.Add(ref sum, partialSum);
-            });
-            Console.WriteLine($"Sum of 1 to 1000 = {sum}");
+            var summary = BenchmarkRunner.Run<Program>();
+            Console.WriteLine(summary);
             // Console.ReadKey();
+        }
+
+        [Benchmark]
+        public void SquareEachValue()
+        {
+            const int count = 1_000_000;
+            var values = Enumerable.Range(0, count);
+            var results = new int[count];
+
+            // Benchmark takes 26.72 ms
+            Parallel.ForEach(values, x => { results[x] = (int)Math.Pow(x, 2); });
+        }
+
+        [Benchmark]
+        public void SquareEachValueChunked()
+        {
+            const int count = 1_000_000;
+            var values = Enumerable.Range(0, count);
+            var results = new int[count];
+
+            // Benchmark takes 7.807 ms
+            var part = Partitioner.Create(0, count, 100_000);
+            Parallel.ForEach(part, range =>
+            {
+                for (int i = range.Item1; i < range.Item2; i++)
+                {
+                    results[i] = (int)Math.Pow(i, 2);
+                }
+            });
         }
     }
 }
