045be3f78f5093f84a76809b104c81d86d33cc55
shocklaing <shockwater@gmail.com>
2018-10-07 16:56:48 +0800

    Basic parallel loop usage.

--- ParallelLoops/Program.cs
+++ ParallelLoops/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ParallelLoops
 {
@@ -6,6 +9,23 @@ namespace ParallelLoops
     {
         static void Main(string[] args)
         {
+            var a = new Action(() => Console.WriteLine($"First {Task.CurrentId}"));
+            var b = new Action(() => Console.WriteLine($"Second {Task.CurrentId}"));
+            var c = new Action(() => Console.WriteLine($"Third {Task.CurrentId}"));
+
+            Parallel.Invoke(a, b, c);
+
+            Parallel.For(1, 11, i =>
+            {
+                Console.WriteLine($"{i * i} \t");
+            });
+
+            var words = new string[] { "oh", "what", "a", "night" };
+            Parallel.ForEach(words, word =>
+            {
+                Console.WriteLine($"{word} has length {word.Length} (task {Task.CurrentId})");
+            });
+
             Console.ReadKey();
         }
     }
