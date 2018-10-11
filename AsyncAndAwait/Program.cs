034f011e9869c1faa05ee776ef1348b0742bd926
shocklaing <shockwater@gmail.com>
2018-10-11 09:29:09 +0800

    Using Task.Run to run async method.

--- AsyncAndAwait/Program.cs
+++ AsyncAndAwait/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace AsyncAndAwait
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var t = Task.Factory.StartNew(async () =>
+            {
+                await Task.Delay(3000);
+                return 123;
+            }).Unwrap();
+
+            Console.WriteLine($"Task.Factory.StartNew result :{t.Result}");
+
+            int result = await Task.Run(async () =>
+            {
+                await Task.Delay(3000);
+                return 456;
+            });
+
+            Console.WriteLine($"Task.Run result:{result}");
         }
     }
 }
